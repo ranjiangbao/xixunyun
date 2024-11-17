bb5c861e82ab98652d7cc5090fa4e118eeb98b4a
ranjiangbao <167847052+ranjiangbao@users.noreply.github.com>
2024-11-17 08:41:10 +0800

    Update xixunyun.py

--- xixunyun.py
+++ xixunyun.py
@@ -21,10 +21,4 @@ data = {
 headers = {'Content-Type': 'application/json'}
 
 response = requests.post(url='https://service-nm4jylpg-1251957121.gz.apigw.tencentcs.com/release/xixunyun', headers=headers, data=json.dumps(data))
-print(response.json()["data"])
-
-SCKEY=os.environ["SCKEY"]
-if len(SCKEY) >= 1:
-  url = 'https://sctapi.ftqq.com/'+SCKEY+'.send'
-  requests.post(url, data={"title": "习讯云签到提醒", "desp": response.json()["data"]})
-
+print(response.json()["data"]
